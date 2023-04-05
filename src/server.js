9fa329ecb29b960f9b9ecbba257851657ff52d76
VuDInhPhong <vudinhphong.261001@gmail.com>
2023-04-05 14:11:43 +0700

    Bai7 DanIT

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+require('dotenv').config();
 const app = express();
-const port = 3000
+const port = process.env.PORT || 8080;
 configViewEngine(app);
-// app.get('/', (req, res) => {
-//     res.send('Hello World! voi phong')
-// })
+
+
 app.get('/', (req, res) => {
     res.render('test/index.ejs')
 })
@@ -18,3 +18,4 @@ app.get('/about', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+console.log('>>>>>> check port: ', port)
